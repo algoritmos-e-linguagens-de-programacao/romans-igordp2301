35e2ba78e28a6fc5dba0bc270744a34bc02d4a93
igordp2301 <igordominguespereira10@gmail.com>
2024-05-28 22:13:27 -0300

    Update romans.py
    versao 1 codigo

--- 01/romans.py
+++ 01/romans.py
@@ -1,7 +1,42 @@
 def int_to_roman(num):
-    # Implemente sua função aqui
-    pass
+  #encoding: utf-8
+def int_to_roman(num):
+    num = int(input("Digite um valor númerico: "))
+    sub = num
+    while sub != 0:
+        if sub == 1:
+            sub = sub - 1
+            print("I", end="")
+        elif sub == 4:
+            sub -= 4
+            print("IV", end="")
+        elif sub < 5:
+            sub = sub - 1
+            print("v", end="")
+        elif sub == 9:
+            sub -= 9
+            print("IX", end="")
+        elif sub < 10:
+            sub = sub - 5
+            print("V", end="")
+        elif sub == 40:
+            sub = sub - 40
+            print("XL", end="")
+        elif sub < 50:
+            sub = sub - 10
+            print("X", end="")
+        elif sub < 100:
+            sub = sub - 50
+            print("L", end="")
+        elif sub < 500:
+            sub = sub - 100
+            print("C", end="")
+        elif sub < 1000:
+            sub = sub - 500
+            print("D", end="")
 
+int_to_roman(0)
+        
 
 def roman_to_int(s):
     # Implemente sua função aqui
