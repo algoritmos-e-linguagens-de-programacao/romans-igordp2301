693bec36171e2396ddc665cbe2b8d1423dc42332
igordp2301 <igordominguespereira10@gmail.com>
2024-06-19 20:34:40 -0300

    Update trips.py
    loop

--- 01/trips.py
+++ 01/trips.py
@@ -1,90 +1,26 @@
-def trips(em_casa, em_work, clima1, resultado):
-    em_casa = 2
-    em_work = 1
-    clima1 = [
-        ["Y", "N"],
-        ["N", "N"],
-        ["Y", "N"],
-        ["N", "Y"],
-        ["Y", "Y"],
-    ]
-    resultado = ""
-
-
-    if clima1[0][0] == "Y" or em_work == 0:
-        em_casa -= 1 #casa 1 e trabalho 2
-        em_work += 1
-        resultado += "Y"
-    else: 
-        resultado += " N"
+def trips(dias, em_casa, em_work, clima1):
     
-    if clima1[0][1] == "Y" or em_casa == 0:
-        em_casa += 1 #casa 1 e trabalho 2
-        em_work -= 1
-        resultado += "Y"
-    else: 
-        resultado += " N,"
+    for i in range(dias):
+            
+        if clima1[i][0] == "Y" or em_work == 0:
+            em_casa -= 1 #casa 1 e trabalho 2
+            em_work += 1
+            clima1[i][0] = "Y"
+        else: 
+            clima1[i][0] = "N"
+        
+        if clima1[i][1] == "Y" or em_casa == 0:
+            em_casa += 1 #casa 1 e trabalho 2
+            em_work -= 1
+            clima1[i][1] = "Y"
+        else: 
+            clima1[i][1] = "N"
+        
+        
     
-    if clima1[1][0] == "Y" or em_work == 0:
-        em_casa -= 1 #casa 1 e trabalho 2
-        em_work += 1
-        resultado += "Y"
-    else: 
-        resultado += " N"
-    
-    if clima1[1][1] == "Y" or em_casa == 0:
-        em_casa += 1 #casa 1 e trabalho 2
-        em_work -= 1
-        resultado += "Y"
-    else: 
-        resultado += " N,"
-
-    if clima1[2][0] == "Y" or em_work == 0:
-        em_casa -= 1 #casa 1 e trabalho 2
-        em_work += 1
-        resultado += " Y"
-    else: 
-        resultado += " N"
-
-    if clima1[2][1] == "Y" or em_casa == 0:
-        em_casa += 1 #casa 1 e trabalho 2
-        em_work -= 1
-        resultado += " Y,"
-    else: 
-        resultado += " N"
-
-    if clima1[3][0] == "Y" or em_work == 0:
-        em_casa -= 1 #casa 1 e trabalho 2
-        em_work += 1
-        resultado += "Y"
-    else: 
-        resultado += " N"
-
-    if clima1[3][1] == "Y" or em_casa == 0:
-        em_casa += 1 #casa 1 e trabalho 2
-        em_work -= 1
-        resultado += " Y,"
-    else: 
-        resultado += " N"
-
-    if clima1[4][0] == "Y" or em_work == 0:
-        em_casa -= 1 #casa 1 e trabalho 2
-        em_work += 1
-        resultado += " Y"
-    else: 
-        resultado += " N"
-
-    if clima1[4][1] == "Y" or em_casa == 0:
-        em_casa += 1 #casa 1 e trabalho 2
-        em_work -= 1
-        resultado += " Y."
-    else: 
-        resultado += " N"
-
-    return resultado
+        return clima1
     
 
-
-resultado = trips(2, 1, [["Y", "N"], ["N", "N"], ["Y", "N"], ["N", "Y"], ["Y", "Y"]], "")
-print(resultado)
+#clima1 = trips(2, 1, [["Y", "N"], ["N", "N"], ["Y", "N"], ["N", "Y"], ["Y", "Y"]], "")
+print(trips(5, 2, 1, [["Y", "N"], ["N", "N"], ["Y", "N"], ["N", "Y"], ["Y", "Y"]]))
     
